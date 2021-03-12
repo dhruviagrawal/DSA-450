c470a1b944392e6c35406675bb271c23b83ecd55
shashank088 <72259097+shashank088@users.noreply.github.com>
2021-03-12 23:36:21 +0530

    Add files via upload

---
+++ index7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>arr[i];
+	}
+
+	int max=arr[0],min=arr[0];
+
+	for(int i=1;i<n;i++)
+	{
+		if(arr[i]>max)
+			max=arr[i];
+		if(arr[i]<min)
+			min=arr[i];
+	}
+	cout<<"Min is "<<min<<endl;
+	cout<<"Max is "<<max<<endl;
+}
